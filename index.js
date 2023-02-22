0d7399159c8f74f91d3c55bc72e9a01b5227e8db
Marco Iannuzzi <marcoiannuzzi@live.com.ar>
2023-02-22 20:12:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let auto = "Corolla";
+console.log(auto);
+console.log(typeof(auto));
+
+auto = "308";
+console.log(auto);
+console.log(typeof(auto));
+
+auto = "C4";
+console.log(auto);
+console.log(typeof(auto));
+
+let nombre = "Marco";
+let apellido = "Iannuzzi";
+console.log(`${nombre} ${apellido}`);
+
+let fruta = "naranja";
+let tipoDeLaVariableFruta = typeof(fruta);
+console.log(tipoDeLaVariableFruta);
+
